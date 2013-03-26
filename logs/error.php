dd18df30b180fce137bb9889cfe276aa04dce98f
Nguyen <Nguyen@192.168.23.70>
2013-03-26 16:50:24 +0700

    Add mobile detect & update lang

--- logs/error.php
+++ logs/error.php
@@ -5,3 +5,9 @@
 
 #Fields: date	time	priority	clientip	category	message
 2013-03-12	03:51:37	INFO	::1	Joomla FAILURE: 	Username and password do not match or you do not have an account yet.
+2013-03-26	04:14:16	INFO	::1	Joomla FAILURE: 	Empty password not allowed
+2013-03-26	04:14:46	INFO	::1	Joomla FAILURE: 	Mật khẩu không được để trống
+2013-03-26	04:15:46	INFO	::1	Joomla FAILURE: 	Mật khẩu không được để trống
+2013-03-26	04:15:53	INFO	::1	Joomla FAILURE: 	Username and password do not match or you do not have an account yet.
+2013-03-26	04:17:03	INFO	::1	Joomla FAILURE: 	Tên đăng nhập và mât khẩu không đúng, hoặc bạn chưa có tài khoản tại Laptrinh.Pro.VN
+2013-03-26	04:21:39	INFO	::1	Joomla FAILURE: 	Tên đăng nhập và mât khẩu không đúng, hoặc bạn chưa có tài khoản tại Laptrinh.Pro.VN
